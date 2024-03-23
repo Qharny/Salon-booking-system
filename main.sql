9e0517b37512ee4c252bb3be036be5cd771f93db
Qharny <cannycc1020@gmail.com>
2024-03-23 14:33:05 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   main.sql
    	new file:   querys.sql

--- main.sql
+++ main.sql
@@ -89,48 +89,6 @@ VALUES
 (5, 'Massage Oil', 20, 15.00);
 
 
--- QUERY
-
-SELECT * FROM Inventory;
-SELECT * FROM Payments;
-SELECT * FROM Appointments;
-SELECT * FROM Customers;
-SELECT * FROM Employees;
-SELECT * FROM Services;
-
-
-
-SELECT e.Name AS EmployeeName
-FROM Employees e
-LEFT JOIN Appointments a ON e.EmployeeID = a.EmployeeID
-WHERE a.AppointmentDateTime NOT BETWEEN '2024-03-21 09:00:00' AND '2024-03-21 10:00:00'
-AND a.ServiceID = 1; -- Replace 1 with the desired service ID
-
-
-SELECT a.AppointmentID, SUM(p.Amount) AS TotalPayment
-FROM Appointments a
-LEFT JOIN Payments p ON a.AppointmentID = p.AppointmentID
-GROUP BY a.AppointmentID;
-
-SELECT c.Name AS CustomerName, a.AppointmentDateTime, s.Name AS ServiceName
-FROM Customers c
-INNER JOIN Appointments a ON c.CustomerID = a.CustomerID
-INNER JOIN Services s ON a.ServiceID = s.ServiceID;
-
-UPDATE Customers
-SET Name = 'John Doe Jr.'
-WHERE CustomerID = 1;
-
-SELECT * FROM Customers;
-
-DELETE FROM Employees
-WHERE EmployeeID = 5;
-SELECT * FROM Employees;
-
-ALTER TABLE Services
-ALTER COLUMN Name NVARCHAR(150);
-SELECT * FROM Services;
-
 
 
 
